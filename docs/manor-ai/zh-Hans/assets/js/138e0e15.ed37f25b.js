a4ffa806b3c6d00b036e7c5e4561e1edd84aed47
KelvinLin1016 <KelvinLin1016@users.noreply.github.com>
2026-08-02 22:35:37 +0000

    deploy: manor-os/manor-cloud@8ef34e83d45e0443bb49dda96789de65f545e22d

---
+++ docs/manor-ai/zh-Hans/assets/js/138e0e15.ed37f25b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmanor_ai_docs=self.webpackChunkmanor_ai_docs||[]).push([[921],{1597(s){s.exports=JSON.parse('{"name":"@easyops-cn/docusaurus-search-local","id":"default"}')}}]);
